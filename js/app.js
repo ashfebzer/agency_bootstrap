f67899d2800e965af0128ceeec17a24184c51b3b
Ashwath <ashwath.k22@gmail.com>
2020-06-08 18:28:58 +0530

    Added files

---
+++ js/app.js
@@ -0,0 +1,10 @@
+
+$(window).on('scroll',function(){
+  var scroll = $(window).scrollTop();
+  console.log(scroll);
+  if(scroll >=50){
+    $(".sticky").addClass("stickyadd");
+  }else{
+    $(".sticky").removeClass("stickyadd");
+  }
+});
