ed04aea26e4f4aef12984d7d9ff12f04c45d8a66
Ashwath <ashfebzer@gmail.com>
2020-06-22 10:19:28 +0530

    Added margins and padding

--- js/app.js
+++ js/app.js
@@ -9,7 +9,6 @@ $(document).ready(function(){
   
   $(window).on('scroll',function(){
     var scroll = $(window).scrollTop();
-    console.log(scroll);
     if(scroll >=50){
       $(".sticky").addClass("stickyadd");
     }else{
